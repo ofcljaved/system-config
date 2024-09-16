d82de13d8c804c307bfd252e55216f0e9db2de6a
ofcljaved <javedofficialme@gmail.com>
2024-09-16 08:48:54 +0530

    chore: configure fish according to fzf and tmux

---
+++ config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+starship init fish | source
+
+function fzf_dir
+    set current_dir (pwd)
+    set dir (find . -type d -not -path '*/.*' | fzf)
+    if test -n "$dir"
+        cd "$dir"
+        if test -n "$TMUX"
+            tmux new-window
+        else
+            tmux
+        end
+        cd "$current_dir"
+    end
+end
+
+bind \cf fzf_dir
+
