bb99bfc8cb2cd25a379888acaec4456c885e3e4d
ofcljaved <javedofficialme@gmail.com>
2024-09-17 17:30:34 +0530

    fix: remove node_modules suggestion

--- config.fish
+++ config.fish
@@ -1,11 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
 starship init fish | source
+export PATH="$PATH:/opt/nvim/"
 
 function fzf_dir
     set current_dir (pwd)
-    set dir (find . -type d -not -path '*/.*' | fzf)
+    set dir (find . -type d -not -path '*/.*' -not -path '*/node_modules*' | fzf)
     if test -n "$dir"
         cd "$dir"
         if test -n "$TMUX"
@@ -19,3 +21,10 @@ end
 
 bind \cf fzf_dir
 
+#------ONLY FOR WSL--------
+function go_home
+    cd ~/../../mnt/c/
+    commandline -f repaint
+end
+
+bind \ch go_home
